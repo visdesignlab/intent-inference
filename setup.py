867200e0f7d5f8face78614ee566d4aaaac5c6a9
Jake Wagoner <jakewagoneredu@gmail.com>
2023-08-01 14:00:06 -0600

    Remove redundant setup files

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup(
-        package_dir={'': 'src'},
-        packages=setuptools.find_packages('src'),
-        include_package_data=True,
-    )
