3be7087c9d26d8f345a5d8a239d2de79070d05b5
Jake Wagoner <jakewagoneredu@gmail.com>
2023-08-01 11:43:26 -0600

    Remove SRC (v0.1.5 test)

--- intent_inference/__init__.py
+++ intent_inference/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .api import compute_predictions, run_predictions
 
--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ import setuptools
 setuptools.setup(
     name="intent-inference",
     packages=['intent_inference'],
-    package_dir={'intent_inference': 'src/intent_inference'},
+    package_dir={'intent_inference': 'intent_inference'},
 )
